70442eb1924bed1c4dd6c3175a3b7efce58fcb50
Sreejan <sreejan147@gmail.com>
2021-11-01 23:56:46 +0530

    feat: connected Quiz page to backend

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Question from "../../components/Question/Question";
 import "./Quiz.css";
-import data from "../../data/sports.json";
 
 function Quiz() {
   const [index, setIndex] = useState(0);
@@ -11,10 +10,10 @@ function Quiz() {
   const score = useRef(0);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
+    fetch(`${process.env.REACT_APP_BASE_URL}/quiz/sports`)
+      .then((res) => res.json())
       .then((jsonData) => {
-        allQuestions.current = data.results;
+        allQuestions.current = jsonData.questions;
         setLoading(false);
       });
   }, []);
@@ -35,7 +34,9 @@ function Quiz() {
         <div className="quiz-category-container">
           <h3 className="quiz-category">Sports</h3>
         </div>
-        {loading ? null : index === allQuestions.current.length ? (
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : index === allQuestions.current.length ? (
           <div>
             <h1>Quiz Finished</h1>
             <br />
@@ -51,9 +52,7 @@ function Quiz() {
             index={index}
             options={allQuestions.current[index].answers}
             nextQuestion={nextQuestion}
-            correctAnswerIndex={
-              allQuestions.current[index].correct_answer_index
-            }
+            correctAnswerIndex={allQuestions.current[index].correctAnswerIndex}
             currScore={score.current}
             setScore={setScore}
           />
