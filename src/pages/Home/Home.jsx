f65fca6343aa15384c70ad09bc7f35a8d309b874
Sreejan <sreejan147@gmail.com>
2021-11-02 21:54:30 +0530

    refactor: refactor & rearrangement

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { isAuthenticated } from "../../utils/auth";
 import "./Home.css";
 
 const Home = () => {
@@ -6,19 +7,21 @@ const Home = () => {
     <div className="home">
       <h1>Quiz App</h1>
       <br />
-      <Link to="/quiz">
-        <button>Go to Sports Quiz</button>
-      </Link>
-      <br />
-      <div>
-        <Link to="/signup">
-          <button>Sign Up</button>
+      {isAuthenticated() ? (
+        <Link to="/quiz">
+          <button>Go to Sports Quiz</button>
         </Link>
-        &nbsp;
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-      </div>
+      ) : (
+        <div>
+          <Link to="/signup">
+            <button>Sign Up</button>
+          </Link>
+          &nbsp;
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
