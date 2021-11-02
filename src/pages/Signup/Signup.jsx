ef81806343613f6831e73455d7704ad52110ad22
Sreejan <sreejan147@gmail.com>
2021-11-02 21:50:55 +0530

    feat: implemented signup functionality

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,27 +1,85 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./Signup.css";
 
 const Signup = () => {
+  const history = useHistory();
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    fetch(`${process.env.REACT_APP_BASE_URL}/signup`, {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const user = {
+          name: data.user.name,
+          email: data.user.name,
+          token: data.token,
+        };
+        localStorage.setItem("user", JSON.stringify(user));
+        setLoading(false);
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
   return (
     <div>
       <h1>Signup</h1>
       <br />
       <br />
-      <form>
-        <label htmlFor="name">Name</label>
-        <br />
-        <input type="text" id="name" />
-        <br />
-        <label htmlFor="email">Email</label>
-        <br />
-        <input type="text" id="email" />
-        <br />
-        <label htmlFor="password">Password</label>
-        <br />
-        <input type="password" id="password" />
-        <br />
-        <br />
-        <button>Sign Up</button>
-      </form>
+      {loading ? (
+        <h3 style={{ textAlign: "center" }}>Loading...</h3>
+      ) : (
+        <form>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <br />
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="text"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <br />
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+          <button type="submit" onClick={handleSubmit}>
+            Sign Up
+          </button>
+        </form>
+      )}
     </div>
   );
 };
